5d8e15d7f32ec9f01395263a9dc285fad93f8c03
Matei.Cristache <Matei.Cristache@asee.io>
2025-10-02 15:34:37 +0300

    Create api for searching by address

--- src/main/java/magis/mundi2025/demo/repository/PropertyRepository.java
+++ src/main/java/magis/mundi2025/demo/repository/PropertyRepository.java
@@ -3,5 +3,9 @@ package magis.mundi2025.demo.repository;
 import magis.mundi2025.demo.model.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    List<Property> findByAddressContainingIgnoreCase(String city);
 }
